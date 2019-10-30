b3ef26a18146f4631fe6a4890454cb42127087df
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2019-10-30 02:30:58 +0200

    Implemented Sniffers Coercer.

--- lib/coersers/sniffers_data_coerser.rb
+++ lib/coersers/sniffers_data_coerser.rb
@@ -4,6 +4,16 @@ require_relative 'base_coerser'
 
 class SniffersDataCoerser < BaseCoerser
   def run
+    result = []
+    @data['sequences'].each do |seq|
+      next unless (route_node = @data['node_times'].find { |item| item[:node_time_id] == seq[:node_time_id] })
 
+      start_time = (@data['routes'].find { |route| route[:route_id] == seq[:route_id] })[:time]
+      result << { start_node: route_node[:start_node].strip,
+                  end_node:   route_node[:end_node].strip,
+                  start_time: start_time,
+                  end_time:   Time.at(start_time.to_f + route_node[:duration_in_milliseconds] / 1000).utc }
+    end
+    result
   end
 end
