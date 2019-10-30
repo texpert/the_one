c90ba1e8a880f86a88f40ffd082848960ee9ba2a
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2019-10-30 02:55:06 +0200

    Implemented Loopholes Coercer.

--- lib/coersers/loopholes_data_coerser.rb
+++ lib/coersers/loopholes_data_coerser.rb
@@ -4,6 +4,17 @@ require_relative 'base_coerser'
 
 class LoopholesDataCoerser < BaseCoerser
   def run
+    result = []
+    node_pairs = @data.dig('node_pairs', 'node_pairs')
+    @data.dig('routes', 'routes').each do |route|
+      route_node_pair = node_pairs.find { |item| item['id'] == route['node_pair_id'] }
+      next unless route_node_pair
 
+      result << { start_node: route_node_pair['start_node'],
+                  end_node:   route_node_pair['end_node'],
+                  start_time: DateTime.parse(route['start_time']).to_time.utc,
+                  end_time:   DateTime.parse(route['end_time']).to_time.utc }
+    end
+    result
   end
 end
